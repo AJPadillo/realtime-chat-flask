e7faf127db42a34cec7be522bcb95cccbfabc32f
AJPadillo <ajpadillo@gmail.com>
2025-01-16 12:44:19 +0100

    agregada logica de request para los formularios

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ socketio = SocketIO(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        code = request.form.get('code')
+        join = request.form.get('join', False)
+        create = request.form.get('create', False)
+
     return render_template('home.html')
 
 if __name__ == '__main__':
